dcafa68e9a8e6be159f346f3f1b3d03d88f25621
neredev <neresdev@gmail.com>
2023-02-26 20:30:54 -0300

    Instância da classe ChessMatch e print da mesma usando o método printBoard da classe UI

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,10 +2,13 @@ package src.application;
 
 import src.boardgame.Board;
 import src.boardgame.Position;
+import src.chess.ChessMatch;
 
 public class Main {
     public static void main(String[] args) {
-        Board board = new Board(8, 8);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+
 
     }
 }
